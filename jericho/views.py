b9c29c9b1ce972761303dd82a45d1434b9104281
Gary Koehl <gkoehl@gmail.com>
2012-08-27 12:08:11 -0500

    Resolving url issues.

--- jericho/views.py
+++ jericho/views.py
@@ -9,7 +9,7 @@ from jericho.models import Post, SiteMessaging
 import urllib
 import urllib2
 
-def index(request, post_id):
+def index(request, post_id=1):
     try:
         site_messaging = SiteMessaging.objects.filter(active=True).order_by('-id')[0]
     except IndexError:
@@ -22,6 +22,8 @@ def index(request, post_id):
             starting_post = Post.objects.filter(image__isnull=False).order_by('-id')[0]
         header_height = starting_post.image.image.height + 85
     except IndexError:
+        starting_post = None
+    except Post.DoesNotExist:
         starting_post = None
 
     if (post_id):
